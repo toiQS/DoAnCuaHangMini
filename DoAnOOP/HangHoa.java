a09322a235ac7cb2dbab433894995715d8a23759
toiQS <88361704+toiQS@users.noreply.github.com>
2021-11-26 20:43:10 +0700

    Update HangHoa.java

--- DoAnOOP/HangHoa.java
+++ DoAnOOP/HangHoa.java
@@ -1,10 +1,10 @@
 package DoAnOOP;
 
-public class HangHoa extends ThongTin{
-    private String NgayNhap;
-    private String HSD;
-    private int SoLuong;
-    private String XuatXu;
+public abstract class HangHoa extends ThongTin{
+    public String NgayNhap;
+    public String HSD;
+    public int SoLuong;
+    public String XuatXu;
     public static int dem = 0;
     public HangHoa(String Ten, String Ma, String NgayNhap, String HSD, int SoLuong, String XuatXu){
         super(Ten, Ma);
@@ -13,33 +13,38 @@ public class HangHoa extends ThongTin{
         this.SoLuong = SoLuong;
         this.XuatXu = XuatXu;
     }
+    
     public String getNgayNhap(){
         return NgayNhap;
     }
     public void setNgayNhap(String NgayNhap){
         this.NgayNhap = NgayNhap;
     }
+
     public String getHSD(){
         return HSD;
     }
     public void setHSD(String HSD){
         this.HSD = HSD;
     }
+
     public int getSoLuong(){
         return SoLuong;
     }
     public void setSoLuong(int SoLuong){
         this.SoLuong = SoLuong;
     }
+
     public String getXuatXu(){
         return XuatXu;
     }
     public void setXuatXu(String XuatXu){
         this.XuatXu = XuatXu;
     }
-    @Override
-    public String toString(){
-        return  dem +".Tên mặt hàng: "+ Ten+"/Mã mặt hàng: " + Ma + "/Ngày nhập hàng: "+ NgayNhap +"/ Hạn sử dụng: "+ HSD +
-                "Số Lượng: " + SoLuong+"/Sản xuất ở: "+ XuatXu;
-    }
+
+    // @Override
+    // public String toString(){
+    //     return  dem +".Tên mặt hàng: "+ Ten+"/Mã mặt hàng: " + Ma + "/Ngày nhập hàng: "+ NgayNhap +"/ Hạn sử dụng: "+ HSD +
+    //             "Số Lượng: " + SoLuong+"/Sản xuất ở: "+ XuatXu;
+    // }
 }
