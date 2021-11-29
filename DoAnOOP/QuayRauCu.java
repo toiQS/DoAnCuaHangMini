03d38c3fcf5dda311036304f9b7cc6874150c3cf
toiQS <88361704+toiQS@users.noreply.github.com>
2021-11-29 12:31:36 +0700

    Update QuayRauCu.java

--- DoAnOOP/QuayRauCu.java
+++ DoAnOOP/QuayRauCu.java
@@ -1,4 +1,7 @@
 package DoAnOOP;
 public class QuayRauCu extends HangHoa{
 
+    public QuayRauCu(String Ten, String Ma, String NgayNhap, String HSD, int SoLuong, String XuatXu) {
+        super(Ten, Ma, NgayNhap, HSD, SoLuong, XuatXu);
+    }
 }
