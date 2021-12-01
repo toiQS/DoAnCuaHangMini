0de345979c8fbf9820abd40cb0db79980d685ce1
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-01 15:49:27 +0700

    Update TestMain.java

--- DoAnOOP/TestMain.java
+++ DoAnOOP/TestMain.java
@@ -3,36 +3,37 @@ package DoAnOOP;
 import java.util.Scanner;
 
 public class TestMain {
+    public static void ChucNang(){
+        System.out.println("Cac chuc nang cu the cua chuong trinh: "); //in ra dòng các chức năng cụ thể của chương trình
+        System.out.println("1:  them"); //thêm
+        System.out.println("2:  xoa");  // Xóa
+        System.out.println("3: sua"); //Sửa
+        System.out.println("4: tim kiem"); //tìm kiếm
+        System.out.println("5: thoat"); //thoát
+    }
     public static void DanhSachNhanVien(){
+
+    }
+    public static void DanhSachHangHoa(){
+
+    }
+    public static void DanhSachQuayA(){
+
+    }
+    public static void DanhSachHangHoaTrongKho(){ //danh sách hàng hóa trong kho(cân nhắc có thể thêm)
 
     }
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         QuanLy NVien = new QuanLy();
-//        String arrNhanVien[];
-//        String Ten,Ma = null;
-//        // String TiepTuc = null;
-//        do{
-            System.out.println("Ten Nhan Vien: ");
-            String Ten = a.nextLine();
-            System.out.println("Ma Nhan Vien: ");
-            String Ma = a.nextLine();
-            System.out.println("Gioi Tinh: ");
-            String GioiTinh = a.nextLine();
-            System.out.println("CCCD: ");
-            int CCCD = a.nextInt();
-            System.out.println("Dia Chi: ");
-            String DiaChi = a.nextLine();
-            System.out.println("Ngay Vao Lam: ");
-            String NgayVaoLam = a.nextLine();
-            System.out.println("SDT: ");
-            String SDT = a.nextLine();
-            // System.out.println("Tiep tuc?: ");
-            Nhanvien nhanvien = new Nhanvien(Ten, Ma, GioiTinh, CCCD, DiaChi, NgayVaoLam, SDT);
+        String type = a.nextLine();
+        ChucNang();
+        while (true){
+            switch (type){
+                case "1": {
 
-            NVien.addNhanVien(nhanvien);
-            
-//        // }while(!TiepTuc.isEmpty());
-//        }while(!Ten.isEmpty());
+                }
+            }
+        }
     }
 }
