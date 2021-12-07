91912a053b30798ff1c0325077ab3c14d98c1900
toiQS <88361704+toiQS@users.noreply.github.com>
2021-12-07 14:29:26 +0700

    Update TestMain.java

--- DoAnOOP/TestMain.java
+++ DoAnOOP/TestMain.java
@@ -3,93 +3,90 @@ package DoAnOOP;
 import java.util.Scanner;
 
 public class TestMain {
-    public static void ChucNang(){
-        System.out.println("Cac chuc nang cu the cua chuong trinh: "); //in ra dòng các chức năng cụ thể của chương trình
-        System.out.println("1:  them"); //thêm
-        System.out.println("2:  xoa");  // Xóa
-        System.out.println("3: sua"); //Sửa
-        System.out.println("4: tim kiem"); //tìm kiếm
-        System.out.println("5: thoat"); //thoát
-    }
-
-    public static void DanhSachNhanVien(){ // Danh sách thông tin dạng có sẵn
-
-    }
-
-    public static void DanhSachHangHoa(){ //Danh sách thông tin hàng hóa có sẵn
-
-    }
-
-    public static void DanhSachQuayA(){ //Danh sách sản phẩm
-
-    }
-
-    public static void DanhSachHangHoaTrongKho(){ //danh sách hàng hóa trong kho(cân nhắc có thể thêm)
-
+    public static void ChucNang(){ // show ra các chức năng cho người dùng
+        System.out.println("cac chuc nang nguoi dung co the tuong tac duoc: ");
+        System.out.println("1 : Them Nhan Vien");
+        System.out.println("2: Xoa Nhan Vien");
+        System.out.println("3: Sua Thong Tin Trong Danh Sach Nhan Vien");
+        System.out.println("4: Xuat Thong Tin");
+        System.out.println("5: Thoat");
     }
 
     public static void main(String[] args) {
-        Scanner a = new Scanner(System.in);
-        QuanLy NVien = new QuanLy();
-        String type = a.nextLine();
-        System.out.println("Chọn thông tin mà bạn muốn xem: "); //xem xét nhu cầu của người dùng
-
-        String chosse = a.nextLine(); // biến nhập thông tin từ bàn phím
-
-        System.out.println("A. Nhân Viên");
-        System.out.println("B. Hang Hóa");
-        switch (chosse) {
-
-            case "1": {
-                //hiện thông tin nhân viên có sẵn
-                ChucNang(); // hiện ra cách chức năng cơ bản người dùng có thể thao tác
-
-                // vòng lặp nhập thông tin để sử dụng list
-                 switch (chosse){
-
-                     case "1": {
-                         System.out.println("Nhập thông tin: ");
-                         while (true) {
+        Scanner sc = new Scanner(System.in);
+        QuanLy DanhSachNhanVien = new QuanLy();
+        String type = sc.nextLine();
 
 
-                             System.out.print("Họ và Tên: ");
-                             String Ten = a.nextLine();
+            System.out.println("1. Nhan vien");
+            switch (type){
+                case "1":{
+                    ChucNang(); //Show chức năng cho người dùng;
+                    while (true){
+                        switch (type){
+                            case "1":{
+                                //nhập thông tin để thêm nhân viên
+                                System.out.println("Ho Ten: ");
+                                String Ten = sc.nextLine();
+                                System.out.println("Ma So Nhan Vien: ");
+                                String Ma = sc.nextLine();
+                                System.out.println("Gioi Tinh");
+                                String GioiTinh = sc.nextLine();
+                                System.out.println("CCCD: ");
+                                int CCCD = sc.nextInt();
+                                System.out.println("Dia Chi:   ");
+                                String DiaChi = sc.nextLine();
+                                System.out.println("Ngay Vao Lam: ");
+                                String NgayVaolam = sc.nextLine();
+                                System.out.println("SDT: ");
+                                String SDT = sc.nextLine();
+                                Nhanvien nhanvien = new Nhanvien(Ten, Ma, GioiTinh, CCCD, DiaChi, NgayVaolam, SDT);
+                                DanhSachNhanVien.addNhanVien(nhanvien);
+                                DanhSachNhanVien.showThongTin();
+                                break;
+                            }
+                            case "2":{
+                                //nhập mã nhân viên mà người dùng muốn xóa
+                                System.out.println("Nhap Ma So Nhan Vien Muon Xoa: ");
+                                String Ma = sc.nextLine();
+                                DanhSachNhanVien.delNhanVien(Ma);
+                                DanhSachNhanVien.showThongTin();
+                                break;
+                            }
+                            case "3": {
+                                //sửa thong tin nhân viên dựa theo thứ tự danh sách
+                                System.out.println("So Thu Tu Trong Danh Sach: ");
+                                int a = sc.nextInt();
+                                System.out.println("Nhap Thong Tin Can Sua Doi");   //Nhập thông tin cần sửa đổi
+                                System.out.println("Ho Ten: ");
+                                String Ten = sc.nextLine();
+                                System.out.println("Ma So Nhan Vien: ");
+                                String Ma = sc.nextLine();
+                                System.out.println("Gioi Tinh");
+                                String GioiTinh = sc.nextLine();
+                                System.out.println("CCCD: ");
+                                int CCCD = sc.nextInt();
+                                System.out.println("Dia Chi:   ");
+                                String DiaChi = sc.nextLine();
+                                System.out.println("Ngay Vao Lam: ");
+                                String NgayVaolam = sc.nextLine();
+                                System.out.println("SDT: ");
+                                String SDT = sc.nextLine();
+                                Nhanvien nhanvien = new Nhanvien(Ten, Ma, GioiTinh, CCCD, DiaChi, NgayVaolam, SDT);
+                                DanhSachNhanVien.SuaThongTinNhanVien(a, nhanvien);
+                                DanhSachNhanVien.showThongTin();
+                                break;
+                            }
+                            case "4":{
+                                System.out.println("Xuat ra Danh Sach Nhan Vien");
+                                DanhSachNhanVien.showThongTin();
+                                break;
+                            }
 
-                             System.out.print("Mã Số Nhân Viên: ");
-                             String Ma = a.nextLine();
-
-                             System.out.print("Giới Tính: ");
-                             String GioiTinh = a.nextLine();
-
-                             System.out.println("Căn Cước Công Dân: ");
-                             int CCCD = a.nextInt();
-
-                             System.out.println("Địa Chỉ: ");
-                             String DiaChi = a.nextLine();
-
-                             System.out.println("Ngày Vào Làm: ");
-                             String NgayVaoLam = a.nextLine();
-
-                             System.out.print("Số Điện Thoại: ");
-                             String SDT = a.nextLine();
-
-                             Nhanvien nhanvien = new Nhanvien(Ten, Ma, GioiTinh, CCCD, DiaChi, NgayVaoLam, SDT);
-                             NVien.addNhanVien(nhanvien);
-                             break;
-                         }
-                     }
-
-                     case "2":{
-
-                     }
-                 }
-
+                        }
+                    }
+                }
             }
 
-            case "2": {
-                //hiện thông tin có sẵn của hàng hóa
-                ChucNang(); // hiện ra cách chức năng cơ bản người dùng có thể thao tác
-            }
-        }
     }
 }
